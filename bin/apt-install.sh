06e273e7b70198ea05a1d861afd795130dca883b
Mehdi Bouaziz <mehdi@skiplabs.io>
2025-01-29 10:16:16 +0100

    Split script as done in Dockerfile

--- bin/apt-install.sh
+++ bin/apt-install.sh
@@ -6,12 +6,8 @@ LLVM_VERSION=15
 
 apt-get update
 apt-get install -q -y wget gnupg
-wget -O - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | apt-key add -
-echo "deb https://deb.nodesource.com/node_22.x nodistro main" >> /etc/apt/sources.list.d/nodejs.list
-wget -O - https://apt.llvm.org/llvm.sh | bash -s -- $LLVM_VERSION
-apt-get install -q -y automake clang-$LLVM_VERSION clang-format-$LLVM_VERSION curl file gawk gcc git jq lld-$LLVM_VERSION llvm-$LLVM_VERSION make nodejs parallel unzip zip
-npm install -g bun
-npm install -g prettier
+wget -O - https://apt.llvm.org/llvm.sh | bash -s -- $LLVM_VERSION # This runs apt-get update
+apt-get install -q -y automake clang-$LLVM_VERSION clang-format-$LLVM_VERSION curl file gawk gcc git jq lld-$LLVM_VERSION llvm-$LLVM_VERSION make parallel unzip zip
 
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION 100
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$LLVM_VERSION 100
@@ -21,3 +17,9 @@ update-alternatives --install /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-$LLVM_VE
 update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-$LLVM_VERSION 100
 update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-$LLVM_VERSION 100
 update-alternatives --install /usr/bin/wasm-ld wasm-ld /usr/bin/wasm-ld-$LLVM_VERSION 100
+
+wget -O - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | apt-key add -
+echo "deb https://deb.nodesource.com/node_22.x nodistro main" >> /etc/apt/sources.list.d/nodejs.list
+apt-get install -q -y nodejs
+npm install -g bun
+npm install -g prettier
